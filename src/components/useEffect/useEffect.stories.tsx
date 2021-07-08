298d565e6e50da22fcbb6790926e812463c82ba2
hrudkouski <aprilshower19@gmail.com>
2021-07-08 12:34:21 +0300

    useEffect (setInterval, setTimeout)

--- src/components/useEffect/useEffect.stories.tsx
+++ src/components/useEffect/useEffect.stories.tsx
@@ -33,9 +33,9 @@ export const SimpleExample1 = () => {
     }, [])
 
     useEffect(() => {
-        console.log('change fake render')
+        console.log('change counter render')
         document.title = counter.toString();
-    }, [fake])
+    }, [counter])
 
     // если не передали зависимость, юзэффект вызывается каждый раз при перерендере страницы
     // а уже если передали зависимость, тогда уже будет срабатывать только первый раз, а далее при изменении зависимости
@@ -44,5 +44,66 @@ export const SimpleExample1 = () => {
         Hello, {counter}, {fake}
         <button onClick={() => setFake(fake + 1)}>+</button>
         <button onClick={() => setCounter(counter + 1)}>+</button>
+    </>
+}
+
+export const SetTimeoutExample = () => {
+    const [counter, setCounter] = useState(1);
+    const [fake, setFake] = useState(1);
+
+    console.log('SetTimeoutExample')
+
+    useEffect( () => {
+
+        setTimeout(() => {
+            console.log('SetTimeout')
+            document.title = counter.toString();
+        }, 1000)
+
+    }, [counter])
+
+    return <>
+        Hello, counter - {counter}, fake - {fake}
+        <button onClick={() => setFake(fake + 1)}>fake+</button>
+        <button onClick={() => setCounter(counter + 1)}>counter+</button>
+    </>
+}
+
+export const SetIntervalExample = () => {
+    const [counter, setCounter] = useState(1);
+    const [fake, setFake] = useState(1);
+
+    console.log('SetIntervalExample')
+
+    useEffect( () => {
+
+        setInterval(() => {
+            console.log('SetInterval - TIK - ' + counter)
+            setCounter((state) => state + 1 )
+        }, 1000)
+
+    }, [])
+
+    return <>
+        Hello, counter - {counter}, fake - {fake}
+    </>
+}
+
+export const Timer = () => {
+    const [counter, setCounter] = useState(1);
+
+    let hours = new Date().getHours();
+    let minutes = new Date().getMinutes();
+    let seconds = new Date().getSeconds();
+
+    useEffect( () => {
+        setInterval(() => {
+            console.log('SetInterval - TIK - ' + counter)
+            setCounter((state) => state + 1 )
+        }, 1000)
+    }, [])
+
+    return <>
+        {`My watch - ${hours}:${minutes}:${seconds}`}
     </>
 }
